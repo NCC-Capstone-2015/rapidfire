22dbe73b14573aeaed630cf7cffcc9ede08f6cbc
DrewOlehy <d.olehy@gmail.com>
2015-06-06 20:59:34 -0500

    Added default route

--- lib/rapidfire/engine.rb
+++ lib/rapidfire/engine.rb
@@ -3,5 +3,8 @@ require 'active_model_serializers'
 module Rapidfire
   class Engine < ::Rails::Engine
     isolate_namespace Rapidfire
+    initializer "rapidfire.load_app_root" do |app|
+       Rapidfire.app_root = app.root
+    end
   end
 end
